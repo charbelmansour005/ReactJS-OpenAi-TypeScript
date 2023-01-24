31b24c8ef843111c5eab8dd248491f834b9bb7bc
charbelmansour005 <charbelmansour005@gmail.com>
2023-01-24 21:36:43 +0200

    added <> better styling

--- src/pages/OpenAI.tsx
+++ src/pages/OpenAI.tsx
@@ -13,7 +13,9 @@ import { createTheme, ThemeProvider } from "@mui/material/styles"
 import { useState } from "react"
 import { randomLoadingText } from "../helpers/loading"
 import Chip from "@mui/material/Chip"
+import "./OpenAI.css"
 import { Player } from "@lottiefiles/react-lottie-player"
+import { makeStyles } from "@mui/styles"
 
 const theme = createTheme()
 
@@ -60,7 +62,8 @@ export default function OpenAI() {
     }
   }
 
-  const LoadingText: string = randomLoadingText
+  // const LoadingText: string = randomLoadingText
+  const classes = useStyles()
 
   return (
     <ThemeProvider theme={theme}>
@@ -72,7 +75,9 @@ export default function OpenAI() {
           sm={4}
           md={5}
           sx={{
-            backgroundImage: "url(https://source.unsplash.com/random)",
+            backgroundImage:
+              "url(https://openai.com/content/images/2022/05/openai-avatar.png)",
+            // backgroundImage: "url(https://source.unsplash.com/random)",
             backgroundRepeat: "no-repeat",
             backgroundColor: (t) =>
               t.palette.mode === "light"
@@ -91,7 +96,7 @@ export default function OpenAI() {
           elevation={3}
           square
           style={{
-            backgroundColor: "#dcdcdc",
+            backgroundColor: "#ffffff",
             borderWidth: 1,
             borderColor: "black",
           }}
@@ -108,7 +113,9 @@ export default function OpenAI() {
               sx={{
                 maxWidth: "90%",
                 width: "90%",
-                backgroundColor: "silver",
+                backgroundColor: "#f7f7f8",
+                // borderColor: "transparent",
+                borderColor: "#c8c9d5",
               }}
               variant="outlined"
               elevation={0}
@@ -119,6 +126,8 @@ export default function OpenAI() {
                   variant="h5"
                   component="div"
                   fontWeight="300"
+                  fontFamily="Inter"
+                  sx={{ color: "#24a37f" }}
                 >
                   {output.length && !loading ? "OpenAI" : null}
                   {!output.length && loading ? (
@@ -138,28 +147,36 @@ export default function OpenAI() {
                 <Divider textAlign="center">
                   {output.length && !loading ? (
                     <Chip
-                      variant="outlined"
-                      color="primary"
-                      label="Output"
-                      sx={{ fontWeight: "bold", color: "black" }}
+                      variant="filled"
+                      label="Output below"
+                      sx={{
+                        fontWeight: "bold",
+                        color: "white",
+                        backgroundColor: "#24a37f",
+                        fontFamily: "Inter",
+                      }}
                     />
                   ) : null}
                 </Divider>
                 <Box sx={{ mt: 2 }}>
                   {error ? (
-                    <Typography variant="body2" color="text.primary">
+                    <Typography
+                      variant="body2"
+                      color="text.primary"
+                      fontFamily="Inter"
+                    >
                       There was an error...
                     </Typography>
                   ) : (
-                    <Typography variant="body2" color="text.primary">
-                      {output.length ? (
-                        output
-                      ) : (
-                        <p>
-                          Give me a <strong>prompt</strong> and let me do the
-                          rest.
-                        </p>
-                      )}
+                    <Typography
+                      variant="body2"
+                      color="text.primary"
+                      fontFamily="Inter"
+                    >
+                      {output.length
+                        ? output
+                        : `Give me a prompt and let me do the
+                          rest.`}
                     </Typography>
                   )}
                 </Box>
@@ -186,14 +203,15 @@ export default function OpenAI() {
                 position: "absolute",
                 bottom: 0,
                 mb: 5,
-                backgroundColor: "silver",
+                backgroundColor: "#f7f7f8",
                 paddingLeft: 10,
                 paddingRight: 10,
                 paddingTop: 1,
                 paddingBottom: 2,
                 borderRadius: 1,
                 border: 1,
-                borderColor: "darkgray",
+                // borderColor: "transparent",
+                borderColor: "#c8c9d5",
               }}
             >
               <TextField
@@ -201,14 +219,21 @@ export default function OpenAI() {
                 fullWidth
                 id="prompt"
                 label="Prompt"
+                placeholder="An apple a day"
                 name="prompt"
-                sx={{ backgroundColor: "silver", width: "100%", mb: 3 }}
+                sx={{
+                  backgroundColor: "#f7f7f8",
+                  width: 300,
+                  mb: 2.5,
+                  fontFamily: "Inter",
+                }}
+                color="secondary"
                 size="small"
                 variant="outlined"
                 onChange={handleChange}
                 autoComplete="off"
               />
-              {/* <Divider sx={{ mb: 2 }}></Divider> */}
+              <Divider light={false} sx={{ mb: 2 }}></Divider>
               <Box
                 sx={{
                   display: "flex",
@@ -219,11 +244,16 @@ export default function OpenAI() {
                 <Button
                   onClick={handleSubmit2}
                   fullWidth
-                  variant="outlined"
-                  color="primary"
+                  variant="contained"
+                  disableElevation
                   disabled={!input || loading}
                   size="small"
-                  sx={{ mt: 1, mb: 1, width: "50%" }}
+                  sx={{
+                    width: "50%",
+                    textTransform: "capitalize",
+                    fontSize: 15,
+                  }}
+                  className={classes.root}
                 >
                   {loading ? "Loading" : "Submit"}
                 </Button>
@@ -235,3 +265,17 @@ export default function OpenAI() {
     </ThemeProvider>
   )
 }
+
+const useStyles = makeStyles({
+  root: {
+    background: "linear-gradient(45deg, #24a37f 30%, #24a37f 90%)",
+    color: "#fff",
+    "&:hover": {
+      backgroundColor: "#fff",
+      color: "#fff",
+    },
+  },
+  rootInput: {
+    borderColor: "orange",
+  },
+})
