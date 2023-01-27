bdc5bde662565ee09ce85fc71e41e01d29d6f1c6
charbelmansour005 <charbelmansour005@gmail.com>
2023-01-27 15:04:50 +0200

    used <> component

--- src/components/OutputCard.tsx
+++ src/components/OutputCard.tsx
@@ -1,6 +1,10 @@
 import { Player } from "@lottiefiles/react-lottie-player"
 import { Box, CardContent, Typography, Card, Divider } from "@mui/material"
 import { randomLoadingText } from "../helpers/loading"
+import UITypography from "./UITypography"
+import CapabilitiesTypography from "./CapabilitiesTypography"
+import CapabilitiesCard from "./CapabilitiesCard"
+import ElectricBoltTwoTone from "@mui/icons-material/ElectricBoltTwoTone"
 
 type Props = {
   output: any
@@ -11,13 +15,16 @@ type Props = {
 }
 
 function OutputCard({ ...props }: Props) {
+  const imageURL =
+    "https://brandlogovector.com/wp-content/uploads/2023/01/ChatGPT-Icon-Logo-PNG.png"
   return (
     <Box
       sx={{
         alignItems: "center",
         justifyContent: "center",
         display: "flex",
-        m: 0.5,
+        flexDirection: "column",
+        m: 1,
       }}
     >
       <Card
@@ -25,27 +32,31 @@ function OutputCard({ ...props }: Props) {
           maxWidth: "100%",
           width: "100%",
           height: "100%",
-          padding: 2,
+          padding: 0,
         }}
         variant="outlined"
-        // elevation={1}
+        elevation={0}
       >
         <CardContent>
           {props.output.length && !props.loading ? (
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="div"
-              fontFamily="Inter"
-              fontWeight="bold"
-              fontSize={20}
-            >
-              {props.model ? props.model : `davinci`}
-            </Typography>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <img
+                style={{
+                  height: 25,
+                  width: 25,
+                  borderRadius: 3,
+                  marginRight: 15,
+                }}
+                src={imageURL}
+              />
+              <UITypography>
+                {props.model ? props.model : `davinci`}
+              </UITypography>
+            </div>
           ) : null}
           {!props.output.length && props.loading ? (
             <Player
-              src="https://assets8.lottiefiles.com/packages/lf20_4hyyiayl.json"
+              src="https://assets3.lottiefiles.com/packages/lf20_E5C0kC.json"
               loop
               style={{ height: 100, width: 100 }}
               speed={1}
@@ -54,16 +65,18 @@ function OutputCard({ ...props }: Props) {
             />
           ) : null}
           {!props.output.length && !props.loading ? (
-            <Typography
-              gutterBottom
-              variant="h5"
-              component="div"
-              fontWeight="bold"
-              fontSize={20}
-              fontFamily="Inter"
-            >
-              OpenAI
-            </Typography>
+            <div style={{ display: "flex", flexDirection: "row" }}>
+              <img
+                style={{
+                  height: 25,
+                  width: 25,
+                  borderRadius: 3,
+                  marginRight: 15,
+                }}
+                src={imageURL}
+              />
+              <UITypography>OpenAI</UITypography>
+            </div>
           ) : null}
           <Divider textAlign="center"></Divider>
           <Box sx={{ mt: 2 }}>
@@ -80,7 +93,6 @@ function OutputCard({ ...props }: Props) {
                 variant="body2"
                 color="text.primary"
                 fontFamily="Inter"
-                whiteSpace="pre-wrap"
                 fontSize={14}
                 sx={{
                   display: "flex",
@@ -91,8 +103,7 @@ function OutputCard({ ...props }: Props) {
                 {!props.output &&
                   !props.loading &&
                   !props.error &&
-                  `Give me a prompt and let me do the
-              rest.`}
+                  `Unlock endless possibilities with the powerful OpenAI Davinci Engine language model`}
                 {!props.output.length && props.loading
                   ? randomLoadingText
                   : props.base + props.output}
@@ -101,6 +112,53 @@ function OutputCard({ ...props }: Props) {
           </Box>
         </CardContent>
       </Card>
+      {!props.output && !props.loading && (
+        <>
+          <Box
+            sx={{
+              mt: "5%",
+              alignItems: "center",
+              justifyContent: "center",
+              display: "flex",
+              flexDirection: "column",
+            }}
+          >
+            <ElectricBoltTwoTone
+              sx={{ alignItems: "center", justifyContent: "center", mb: 1 }}
+              fontSize="small"
+            />
+            <UITypography>Capabilities</UITypography>
+          </Box>
+          <Box
+            sx={{
+              maxWidth: "100%",
+              width: "100%",
+              height: "100%",
+
+              justifyContent: "center",
+              alignItems: "center",
+              display: "flex",
+              flexDirection: "row",
+            }}
+          >
+            <CapabilitiesCard>
+              <CapabilitiesTypography>
+                Text generation for card captions or descriptions.
+              </CapabilitiesTypography>
+            </CapabilitiesCard>
+            <CapabilitiesCard>
+              <CapabilitiesTypography>
+                Image captioning for descriptive alt text.
+              </CapabilitiesTypography>
+            </CapabilitiesCard>
+            <CapabilitiesCard>
+              <CapabilitiesTypography>
+                Text summarization for concise summaries on a card.
+              </CapabilitiesTypography>
+            </CapabilitiesCard>
+          </Box>
+        </>
+      )}
     </Box>
   )
 }
