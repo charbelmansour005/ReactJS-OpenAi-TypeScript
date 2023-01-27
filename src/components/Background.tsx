dad89a087925b19cfdded6d82c46a57728701949
charbelmansour005 <charbelmansour005@gmail.com>
2023-01-27 15:04:00 +0200

    removed <> unused bg

--- src/components/Background.tsx
+++ src/components/Background.tsx
@@ -1,27 +1,9 @@
-import { Button, Card } from "@mui/material"
 import Grid from "@mui/material/Grid"
 
 type Props = {}
 
 function Background({}: Props) {
-  return (
-    <Grid
-      item
-      // xs={false}
-      sm={0}
-      // md={5}
-      sx={{
-        backgroundImage: "url(https://source.unsplash.com/random)",
-        backgroundRepeat: "no-repeat",
-        backgroundColor: (t) =>
-          t.palette.mode === "light" ? t.palette.grey[50] : t.palette.grey[900],
-        backgroundSize: "cover",
-        backgroundPosition: "center",
-        color: "green",
-        overflow: "hidden",
-      }}
-    ></Grid>
-  )
+  return <Grid item sm={0}></Grid>
 }
 
 export default Background
